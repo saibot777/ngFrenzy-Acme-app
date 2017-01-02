b5f5755a2d62f1523b1352af98ddc1cc0a805886
Stefan <stefantrajkovic777@gmail.com>
2017-01-02 01:16:07 +0100

    input, output, event emitters added...

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -77,4 +77,8 @@ export class ProductListComponent implements OnInit {
     this.showImage = !this.showImage;
   }
 
+  onRatingClicked(message: string): void {
+    this.pageTitle = 'Product List: ' + message;
+  }
+
 }
--- src/app/shared/star.component.ts
+++ src/app/shared/star.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnChanges } from '@angular/core';
+import { Component, OnChanges, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'ai-star',
@@ -9,11 +9,17 @@ export class StarComponent implements OnChanges {
 
   constructor() { }
 
-  rating: number = 4;
+  @Input() rating: number;
   starWidth: number;
+@Output() ratingClicked: EventEmitter<string> =
+  new EventEmitter<string>();
 
   ngOnChanges(): void {
     this.starWidth = this.rating * 100/5;
+  }
+
+  onClick(): void {
+    this.ratingClicked.emit(`The rating ${this.rating} was clicked!`);
   }
 
 }
