d2371e710ffd99f692ff63bd5c5541a935afbf72
saibot777 <stefantrajkovic777@gmail.com>
2017-01-04 14:29:26 +0100

    code-based navigation implemented...

--- src/app/products/product-detail/product-detail.component.ts
+++ src/app/products/product-detail/product-detail.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 import { IProduct } from '../product';
 
@@ -7,9 +8,21 @@ import { IProduct } from '../product';
   templateUrl: './product-detail.component.html',
   styleUrls: ['./product-detail.component.css']
 })
-export class ProductDetailComponent {
+export class ProductDetailComponent implements OnInit {
 
   pageTitle: string = 'Product Detail';
   product: IProduct;
+
+  constructor(private _route: ActivatedRoute,
+              private _router: Router) { }
+
+  ngOnInit(): void {
+    let id = +this._route.snapshot.params['id'];
+    this.pageTitle += `: ${id}`;
+  }
+
+  onBack(): void {
+    this._router.navigate(['/products']);
+  }
 
 }
