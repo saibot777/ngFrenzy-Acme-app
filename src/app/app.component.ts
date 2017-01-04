4bee0587d49ad78569c777f23290cff6f1571b46
saibot777 <stefantrajkovic777@gmail.com>
2017-01-04 13:41:32 +0100

    nav with routes added, layout refactored

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 
+import { ProductService } from './products/product.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [ ProductService ]
 })
 export class AppComponent {
   pageTitle: string = 'ngFrenzy Product Managment';
